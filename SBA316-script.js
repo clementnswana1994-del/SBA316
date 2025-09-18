3a3bf772c68a0167bb9223120db3a433c61098b1
clementnswana1994-del <clementnswana1994@gmail.com>
2025-09-18 18:39:08 -0400

    first commit

--- SBA316-script.js
+++ SBA316-script.js
@@ -1,5 +1,41 @@
-import Dashborad from "./Dashborad.js";
-import AbstractView from "./AbstractView.js";
+export default class extends AbstractView {
+    constructor() {
+        super();
+        this.setTitle("Dashboard");
+    }
+    
+    async getHtml() {
+        return `
+           <h1>Welcome back, Home</h1>
+           <p>
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+               Quisque rutrum libero non elementum commodo. Pellentesque eget egestas tellus.
+               Vestibulum pellentesque finibus dolor, id euismod odio viverra et. Vivamus congue
+               consectetur pharetra. Fusce id porta magna. In sagittis malesuada enim at
+               rutrum. Ut fringilla ligula interdum mauris maximus, sed accumsan sapien
+               placerat. Duis id nisl vel nulla rutrum sollicitudin ac sit amet tellus.
+               Nam rutrum aliquam placerat. Curabitur tempor, turpis vitae bibendum
+               finibus, nibh augue ultricies tellus, nec porta mauris nisl id mauris.
+               Pellentesque sit amet risus id ipsum semper ultricies non in ipsum.
+            </p>
+            <p>
+               <a href="/posts" data-link>View recent posts</a>
+            </p>
+        `;
+    }
+};
+
+export default class {
+     constructor() {
+ }
+
+ setTitle(title) {
+     document.title = title;
+ }
+ async getHtml() {
+     return "";
+   }
+ }
 
 const navigateTo = url => {
     history.pushState(null, null, url);
